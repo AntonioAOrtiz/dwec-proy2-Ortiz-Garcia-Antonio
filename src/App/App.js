5a5450a96c1ebfd5c67070ff372497be00b766ac
AntonioAOrtiz <aaortiz.alumno@iesrafaelalberti.es>
2022-02-08 21:43:16 +0100

    navbar

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './Header/Header';
+import Header from './Header';
 import Utility from './Utility/Utility';
 import About from './About';
 import Portfolio from './Portfolio';
---
+++ src/App/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header id="header">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#header">PORTFOLIO ANTONIO ORTIZ</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#about">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#academics">Academics</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#portfolio">Portfolio</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#utility">Utility</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+
+export default Header;
