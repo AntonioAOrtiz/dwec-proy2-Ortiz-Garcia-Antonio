d234667423e6b85a90ca50987102b304d2e4378a
AntonioAOrtiz <aaortiz.alumno@iesrafaelalberti.es>
2022-02-11 19:52:19 +0100

    modificación about

--- src/App/About.js
+++ src/App/About.js
@@ -5,7 +5,7 @@ function About(){
         <section id="about">
             <div className="container-fluid p-5 my-5 bg-primary text-center text-white">
                 <h1 className="display-4">Hola, soy Antonio Ortiz, Desarrollador Web</h1>
-                <p className="mt-5 text-white">Me dedico a la programación y al desarrollo web full stack. Busco ampliar mis conocimientos y habilidades para ponerlos en práctica entornos de trabajo. </p>
+                <p className="mt-5 text-white"><strong>Me dedico a la programación y al desarrollo web full stack. Busco ampliar mis conocimientos y habilidades para ponerlos en práctica entornos de trabajo.</strong></p>
             </div>
         </section>
     )
