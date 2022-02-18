6eab271549f75a171903e98e7ab878a4d4688b0a
AntonioAOrtiz <aaortiz.alumno@iesrafaelalberti.es>
2022-02-18 16:26:16 +0100

    formulario terminado

--- src/App/Academics.js
+++ src/App/Academics.js
@@ -6,9 +6,9 @@ function Academics() {
             <div className="container p-4 my-4 text-left text-body">
                 <h2 className="display-5 text-center"><u>Formación académica</u></h2>
                 <ul className="list-group p-3">
-                    <li className="list-group-item bg-primary text-center"><strong>C.F.G.S. en Desarrollo de Aplicaciones Web.</strong></li>
-                    <li className="list-group-item bg-primary text-center">C.F.G.S. en Vitivinicultura.</li>
-                    <li className="list-group-item bg-primary text-center">Bachillerato en Ciencias de la Naturaleza y Salud.</li>
+                    <li className="list-group-item bg-primary text-center text-white"><strong>C.F.G.S. en Desarrollo de Aplicaciones Web.</strong></li>
+                    <li className="list-group-item bg-primary text-center text-white">C.F.G.S. en Vitivinicultura.</li>
+                    <li className="list-group-item bg-primary text-center text-white">Bachillerato en Ciencias de la Naturaleza y Salud.</li>
                 </ul>
             </div>
         </section>
--- src/App/Footer/Contact.js
+++ src/App/Footer/Contact.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { useForm } from "react-hook-form";
 
 
 const Contact = () => {
     
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [message, setMessage] = useState('');
+    const { register, handleSubmit,reset, formState: {errors} } = useForm();
 
-    const onSubmit = data => console.log(data);
+    const onSubmit = (data, e) => {
+        e.preventDefault();
+        console.log(data);
+        setMessage(`Gracias ${data.nombre}, por tu mensaje.`);
+        reset();
+      };
+    
 
     const emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
     const nombreRegex = /^[a-zA-Z][0-9a-zA-Z .,'-]*$/;
@@ -18,9 +25,10 @@ const Contact = () => {
                 <h3>Contacto</h3>
                 <form className="grid" id="contact" onSubmit={handleSubmit(onSubmit)}>
                     <div className="mt-3 mb-3">
-                        <input id="nombre" type="text" placeholder="Nombre" className="form-control"  {...register("nombre",{required: true, pattern:nombreRegex})} />
+                        <input id="nombre" type="text" placeholder="Nombre" className="form-control"  {...register("nombre",{required: true, minLength:2, pattern:nombreRegex})} />
                         {errors.nombre && errors.nombre.type === "required" && <span>Campo obligatorio</span>}
                         {errors.nombre && errors.nombre.type === "pattern" && <span>El nombre no es válido </span>}
+                        {errors.nombre && errors.nombre.type === "minLength" && <span>El nombre debe tener como mínimo dos caracteres </span>}
                     </div>
                     <div className="mt-3 mb-3">
                         <input id="apellido" type="text" placeholder="Apellido" className="form-control" {...register("apellido",{required: true, pattern:nombreRegex})} />
@@ -33,13 +41,15 @@ const Contact = () => {
                         {errors.email && errors.email.type === "pattern" && <span>Email no válido</span>}
                     </div>
                     <div className="mt-3 mb-3">
-                        <textarea id="mensaje" type="textarea" placeholder="Mensaje" className="form-control" rows="5" {...register("mensaje",{required: true,maxLength:255, pattern: mensajeRegex})} />
+                        <textarea id="mensaje" type="textarea" placeholder="Mensaje" className="form-control" rows="5" {...register("mensaje",{required: true,maxLength:255,minLength:10, pattern: mensajeRegex})} />
                         {errors.mensaje && errors.mensaje.type === "required" && <span>Campo obligatorio</span>}
                         {errors.mensaje && errors.mensaje.type === "maxLength" && <span>El mensaje debe tener como máximo 255 caracteres</span>}
+                        {errors.mensaje && errors.mensaje.type === "minLength" && <span>El mensaje debe tener como mínimo 10 caracteres</span>}
                         {errors.mensaje && errors.mensaje.type === "pattern" && <span>Mensaje no válido</span>}
                     </div>
-                    <button type="submit" className="btn btn-primary">Enviar</button>
+                    <button id="submit" type="submit" className="btn btn-primary m-2">Enviar</button>
                 </form>
+                {message}
         </div>    
     );
 }
