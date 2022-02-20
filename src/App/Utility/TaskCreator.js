814c853cb23efa136ff99f08691bef92ed18350d
AntonioAOrtiz <aaortiz.alumno@iesrafaelalberti.es>
2022-02-20 19:41:14 +0100

    creador de nuevas tareas

--- src/App/Utility/TaskCreator.js
+++ src/App/Utility/TaskCreator.js
@@ -1,1 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+// Componente que crea tareas.
+const TaskCreator = props => {
+    
+    // Estado que va a cambiar de cadena vacía con la tarea que va a pasar e usuario.
+    const [newTaskName, setNewTaskName] = useState("");
+    
+    // Función que tiene un evento que va a ir cambiando el valor según lo que esté escribiendo el usuario.
+    const updateNewTaskValue = e => setNewTaskName(e.target.value);
+
+    // Función que va a crear las tareas.
+    const createNewTask = () => {
+        props.callback(newTaskName);
+        setNewTaskName("");
+    }
+
+    return(
+        <div className="my-1">
+            <input 
+            type="text"
+            className="form-control"
+            value={newTaskName}
+            onChange={updateNewTaskValue}
+            />
+            <button className="btn btn-primary mt-1" onClick={createNewTask}>
+                Nueva Tarea
+            </button>
+        </div>
+    )
+}
+
+export default TaskCreator;
--- src/App/Utility/Utility.js
+++ src/App/Utility/Utility.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import TaskRow from './TaskRow';
 import TaskBanner from './TaskBanner';
+import TaskCreator from './TaskCreator';
 
 function Utility() {
 
@@ -12,10 +13,17 @@ function Utility() {
         { name: "Tarea Cuatro", done: false }
     ]);
 
+    // Función que recibe el nombre de una tarea nueva y la agrega a la lista de tareas si no fue ya agregada.
+    const createNewTask = taskName => {
+        if (!taskItems.find(t => t.name === taskName)){
+            setTaskItems([...taskItems, {name: taskName, done: false}])
+        }
+    }
+
     //Recibe una tarea y por cada tarea que reciba cambia el estado de los items.
     const toggleTask = task =>
         //Recorro la lista de tareas y si el nombre coincide con la tarea que están dando, cambia el valor done de la tarea.
-        setTaskItems(taskItems.map(t => (t.name == task.name ? {...t, done: !t.done} : t)));
+        setTaskItems(taskItems.map(t => (t.name === task.name ? {...t, done: !t.done} : t)));
 
     //Función que recorre las tareas y las muestra en pantalla generando múltiples rows.
     const TaskTableRows = () => 
@@ -23,11 +31,13 @@ function Utility() {
             <TaskRow task={task} key={task.name} toggleTask ={toggleTask} />
         ))
     
-
+    
 
     return (
         <section id="utility">
             <TaskBanner userName={userName} taskItems={taskItems} />
+
+            <TaskCreator callback={createNewTask} />
             <table className="table table-striped table-bordered">
                 <thead>
                     <tr>
