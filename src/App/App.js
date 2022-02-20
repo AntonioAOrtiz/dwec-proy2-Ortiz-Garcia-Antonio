a527f291e5a2a1ab1362d5546a2dc21f4ee7a52a
AntonioAOrtiz <aaortiz.alumno@iesrafaelalberti.es>
2022-02-20 18:55:12 +0100

    Lista de tareas en Utility, tabla que muestre las tareas, banner y componentes row y multiple row

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './Header';
-import Utility from './Utility/Utility';
+import Utility from "./Utility/Utility";
 import About from './About';
 import Portfolio from './Portfolio';
 import Footer from './Footer/Footer';
